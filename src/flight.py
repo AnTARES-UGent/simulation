9442d568ccc97f071766bd1ce8dc169907bc7f18
VicKnightDR <vicderidder@hotmail.com>
2024-04-12 02:06:51 +0200

    Add cfg file propagation

--- src/flight.py
+++ src/flight.py
@@ -3,6 +3,22 @@
 from rocketpy import Flight
 from src import environment, rocket
 
-test_flight = Flight(
-  rocket=rocket.calisto, environment=environment.environment, rail_length=5.2, inclination=80, heading=0, verbose=True
-)
+
+
+
+def create_flight(cfg_file):
+   """
+   Arguments:
+
+   Returns:
+
+   """
+
+
+   calisto = rocket.create_rocket(cfg_file)
+   envir = environment.create_environment(cfg_file)
+
+
+   test_flight = Flight(rocket=calisto, environment=envir, rail_length=5.2, inclination=80, heading=0, verbose=True)
+
+   return test_flight
--- src/motor.py
+++ src/motor.py
@@ -2,22 +2,26 @@
 
 from rocketpy import SolidMotor
 
-Pro75M1670 = SolidMotor(
-    thrust_source="../data/motors/Cesaroni_7312M4770-P.eng",
-    dry_mass=2.924,
-    dry_inertia=(0.249, 0.249, 0.351),
-    center_of_dry_mass_position=0.317,
-    grains_center_of_mass_position=0.274,
-    burn_time=1.617,
-    grain_number=3,
-    grain_separation=0.005,
-    grain_density=1815,
-    grain_outer_radius=0.033,
-    grain_initial_inner_radius=0.015,
-    grain_initial_height=0.12,
-    nozzle_radius=0.033,
-    throat_radius=0.011,
-    interpolation_method="linear",
-    nozzle_position=0,
-    coordinate_system_orientation="nozzle_to_combustion_chamber",
-)
+def get_motor(cfg):
+
+   Pro75M1670 = SolidMotor(
+       thrust_source="./data/motors/Cesaroni_7312M4770-P.eng",
+       dry_mass=2.924,
+       dry_inertia=(0.249, 0.249, 0.351),
+       center_of_dry_mass_position=0.317,
+       grains_center_of_mass_position=0.274,
+       burn_time=1.617,
+       grain_number=3,
+       grain_separation=0.005,
+       grain_density=1815,
+       grain_outer_radius=0.033,
+       grain_initial_inner_radius=0.015,
+       grain_initial_height=0.12,
+       nozzle_radius=0.033,
+       throat_radius=0.011,
+       interpolation_method="linear",
+       nozzle_position=0,
+       coordinate_system_orientation="nozzle_to_combustion_chamber",)
+
+
+   return Pro75M1670
