50dc8717ed37e4e3a841a7c22bddfc6f55fa1a5a
Bram Windey <windey.bram@gmail.com>
2024-04-17 20:45:05 +0200

    FEAT: Set rail inclination on 84 degrees, as Euroc prescribes

--- src/flight.py
+++ src/flight.py
@@ -15,6 +15,6 @@ def create_flight(cfg_file):
     calisto = rocket.create_rocket(cfg_file)
     envir = environment.create_environment(cfg_file)
 
-    test_flight = Flight(rocket=calisto, environment=envir, rail_length=5.2, inclination=80, heading=0, verbose=True)
+    test_flight = Flight(rocket=calisto, environment=envir, rail_length=5.2, inclination=84, heading=0, verbose=True)
 
     return test_flight
